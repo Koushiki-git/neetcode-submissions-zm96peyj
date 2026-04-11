91a7f19946b42c7de8e1e00dcaf8dcbb8aebf6f3
Koushiki-git <koushikisingha@gmail.com>
2026-04-11 20:49:19 +0530

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int cnt=0;
+        while(n){
+            n=n&(n-1);
+            cnt++;// no of steps
+        }
+        return cnt;
+
+    }
+};

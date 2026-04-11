e2dfd868e55e9cbd0a19d03edd44c402ee84a377
Koushiki-git <koushikisingha@gmail.com>
2026-04-11 21:28:02 +0530

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int miss=n;
+        for(int i=0;i<n;i++){
+            miss^=i;
+            miss^=nums[i];
+        }
+        return miss;
+        
+    }
+};
